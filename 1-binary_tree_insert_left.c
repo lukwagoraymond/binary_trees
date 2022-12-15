1b4e509abd0024cc0f1442a21564d68b431f1f70
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-12-15 15:53:55 +0300

    Refacted Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,10 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newnode;
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *newnode;
 
 	newnode = binary_tree_node(parent, value);
 
