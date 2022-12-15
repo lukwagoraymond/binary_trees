bb1ec351333d5cedc2755cb46dac897a7e15ac05
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-12-15 16:48:44 +0300

    Task 6 Correction

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -14,8 +14,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && fun)
 	{
-	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
