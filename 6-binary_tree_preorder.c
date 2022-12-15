64278ef6bc2df121361532351bb54c96dcb582aa
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-12-15 16:50:58 +0300

    Task 6 Correction

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_preorder -	Goes through a binary tree using pre-order
-*							traversal
-* @tree:					Pointer to the root node of the tree to
-*							travesal
-* @func:					Pointer to a function to call for each node
-*							The value in the node must be passed as a
-*							parameter to this function
-* Description:				If tree or func is NULL do nothing
+* binary_tree_preorder - Goes through a binary tree using pre-order
+*						traversal
+* @tree: Pointer to the root node of the tree to
+*						travesal
+* @func: Pointer to a function to call for each node
+*		The value in the node must be passed as a
+*		parameter to this function
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
