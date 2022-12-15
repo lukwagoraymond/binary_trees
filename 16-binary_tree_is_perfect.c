aa09e7936ea0a6a7a7deaef4fcda853f8b547791
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-12-15 20:38:43 +0300

    Included binary_tree_size

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -45,3 +45,27 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (length_left > length_right ? length_left : length_right);
 }
+
+/**
+* binary_tree_size - Measures the Size of a binary Tree
+* @tree: A pointer to the root node of the tree to measure the size
+* Return: Returns Size of the Tree
+* Description: Tree is NULL, your function must be (0)
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree)
+	{
+		count += 1;
+		count += binary_tree_size(tree->left);
+		count += binary_tree_size(tree->right);
+	}
+
+	return (count);
+}
